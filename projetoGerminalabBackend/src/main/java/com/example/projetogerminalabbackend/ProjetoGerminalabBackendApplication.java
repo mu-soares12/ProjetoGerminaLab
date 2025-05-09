f5b73a9d9d372900d60a3436d375f7497d21f45e
Murilo Soares <murilo.soares@picpay.com>
2025-05-09 15:07:00 -0300

    Criando projeto Spring

---
+++ projetoGerminalabBackend/src/main/java/com/example/projetogerminalabbackend/ProjetoGerminalabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetogerminalabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGerminalabBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoGerminalabBackendApplication.class, args);
+    }
+
+}
