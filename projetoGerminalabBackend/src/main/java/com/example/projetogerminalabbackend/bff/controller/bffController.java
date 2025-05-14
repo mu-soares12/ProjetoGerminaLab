b57a0e0669b69977d5382d907a95230e08064023
Joao Pereira <joao.vargas@picpay.com>
2025-05-14 17:07:33 -0300

    bff TALVEZ finalizado

--- projetoGerminalabBackend/src/main/java/com/example/projetogerminalabbackend/bff/controller/bffController.java
+++ projetoGerminalabBackend/src/main/java/com/example/projetogerminalabbackend/bff/controller/bffController.java
@@ -1,40 +1,41 @@
 package com.example.projetogerminalabbackend.bff.controller;
 
-import com.example.projetogerminalabbackend.bff.model.alunoDTO;
+import com.example.projetogerminalabbackend.model.Aluno;
+import com.example.projetogerminalabbackend.repository.AlunoRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
-import java.util.ArrayList;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/api/")
 @CrossOrigin(origins = "http://localhost:4200")
 public class bffController {
+    public float media;
+
+    private final AlunoRepository alunoRepository;
+
+    public bffController(AlunoRepository alunoRepository) {
+        this.alunoRepository = alunoRepository;
+    }
 
     @GetMapping("/tabela")
-    public ArrayList<alunoDTO> listarAluno() {
-        ArrayList<alunoDTO> alunoList = new ArrayList<>();
-        alunoDTO aluno = new alunoDTO();
+    public List<Aluno> listarAluno() {
+        List<Aluno> alunos = alunoRepository.findAll();
 
-        aluno.nomeCompleto = "Joao Rosa";
-        aluno.email = "joao@gmail.com";
-        aluno.ano = 2;
-        aluno.serie = 'A';
-        aluno.notaExatas = 10.0f;
-        aluno.notaLinguagens = 9.0f;
-        aluno.notaCiencias = 5.0f;
-        aluno.mediaGeral = (aluno.notaLinguagens + aluno.notaCiencias + aluno.notaExatas) /3;
-        if(aluno.mediaGeral >= 7) {
-            aluno.statusFinal = true;
+        for (Aluno aluno : alunos) {
+            media = (aluno.getNotaExatas() + aluno.getNotaLinguagens() + aluno.getNotaCiencias()) / 3;
+            aluno.setMediaGeral(media);
+            if (aluno.getMediaGeral() >= 7){
+                aluno.setStatusFinal("Aprovado");
+            }
+            else {aluno.setStatusFinal("Reprovado");}
         }
-        else aluno.statusFinal = false;
 
-        alunoList.add(aluno);
-
-        return alunoList;
+        return alunos;
     }
 }
