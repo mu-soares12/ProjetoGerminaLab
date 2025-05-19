cdac2deae06bfbae89dfc4fdafe8917a8f0afe41
Murilo Soares <murilo.soares@picpay.com>
2025-05-19 17:12:10 -0300

    arrumando usuario

--- projetoGerminalabBackend/src/main/java/com/example/projetogerminalabbackend/repository/UsuarioRepository.java
+++ projetoGerminalabBackend/src/main/java/com/example/projetogerminalabbackend/repository/UsuarioRepository.java
@@ -3,5 +3,8 @@ package com.example.projetogerminalabbackend.repository;
 import com.example.projetogerminalabbackend.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByEmailAndSenha(String email, String senha);
 }
